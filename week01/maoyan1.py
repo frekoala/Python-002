59f5dad93634a32a989d2d395ccdbf47358c87b8
frekoala <759743534@qq.com>
2020-07-25 13:01:19 +0800

    homework1 done

--- week01/maoyan1.py
+++ week01/maoyan1.py
@@ -8,7 +8,8 @@ def get_movies_info(myurl):
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
 
-    cookie = 'BIDUPSID=0E70B0EE7251C1784CE3E2EEBB75E826; PSTM=1593183705; BAIDUID=0E70B0EE7251C17844E93FD4E3C80424:FG=1; HMACCOUNT=AF056519A25710AD; BDUSS=mdDTEl1RGxYYlFNSVB0SThMV1V-bXFVYn5ZTXNpUVoxTUd5V0ljNGZFWn5jVEpmSVFBQUFBJCQAAAAAAAAAAAEAAADOiaITyrW8-dbQtcS~vMCtAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH~kCl9~5Apfd; HMACCOUNT_BFESS=AF056519A25710AD; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; delPer=0; PSINO=1; H_PS_PSSID=32294_1431_32361_32045_32116; HMVT=6bcd52f51e9b3dce32bec4a3997715ac|1595609764|'
+    cookie = 'BIDUPSID=0E70B0EE7251C1784CE3E2EEBB75E826; PSTM=1593183705; \
+        BAIDUID=0E70B0EE7251C17844E93FD4E3C80424:FG=1; HMACCOUNT=AF056519A25710AD; BDUSS=mdDTEl1RGxYYlFNSVB0SThMV1V-bXFVYn5ZTXNpUVoxTUd5V0ljNGZFWn5jVEpmSVFBQUFBJCQAAAAAAAAAAAEAAADOiaITyrW8-dbQtcS~vMCtAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH~kCl9~5Apfd; HMACCOUNT_BFESS=AF056519A25710AD; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; delPer=0; PSINO=1; H_PS_PSSID=32294_1431_32361_32045_32116; HMVT=6bcd52f51e9b3dce32bec4a3997715ac|1595609764|'
 
     header = {'user-agent': user_agent, 'cookie': cookie}
     response = requests.get(myurl, headers=header)
@@ -28,7 +29,7 @@ def get_movies_info(myurl):
 
         mylist = [file_name, movie_type, plan_date]
 
-        return mylist  
+        return mylist
 
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
@@ -57,13 +58,12 @@ print(urls)
 mydict = {"电影名称": [], "电影类型": [], "上映时间": []}
 for page in urls:
     list_tmp = get_movies_info(page)
+    assert list_tmp, f"获取电影信息失败,请检查是否验证:{list_tmp}"
     mydict["电影名称"].append(list_tmp[0])
     mydict["电影类型"].append(list_tmp[1])
     mydict["上映时间"].append(list_tmp[2])
     sleep(5)
 
 movie1 = pd.DataFrame(data=mydict)
-movie1.to_csv('movie1.csv',
-                      encoding='gbk',
-                      index=False,
-                      header=False)
+movie1.index += 1
+movie1.to_csv('movie1.csv', encoding='gbk', index=True, header=True)
